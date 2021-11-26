2cab7c618f186d70dcb6fe29316f291d16cc7226
daysemaroto <dmaroto@espol.edu.ec>
2021-11-25 22:18:29 -0500

    init

--- loly_app/main_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('generar-voz/', views.generar_voz, name='generar_voz'),
-]
--- script5.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source /Users/adriantomala/Desktop/TTS-main/venv/bin/activate; 
-python3 /Users/adriantomala/Desktop/TTS-main/TTS/bin/loly/synthesize_loly.py --config_path /Users/adriantomala/Desktop/TTS-main/TTS/bin/loly/config.json --model_path /Users/adriantomala/Desktop/TTS-main/TTS/bin/loly/maldy.pth.tar  --out_path /Users/adriantomala/Desktop/TTS-main/TTS/bin/loly/audios --json_path /Users/adriantomala/Desktop/TTS-main/TTS/bin/loly/json; 
-deactivate;
